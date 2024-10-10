9633e69fd7326ff43cfe3efda2b804140ea88340
Douglas <141868800+cultivador@users.noreply.github.com>
2024-10-10 16:49:06 -0300

    11

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,34 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'master duel"efeito de monstro" ',
+    'quantas invocação seu oponente precisa fazer para ativar o efeito do NIBIRU?',
+    '5'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'master duel "efeito de monstro',
+    'O "C"maxxé ativo quando?',
+    'enviado para o cemiterio'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'master duel "efeito de magia',
+    'O efeito do regeki é?',
+    'destruir todos os monstrps dp oponente)'
+) 
+criaCartao(
+    'master duel "efeito de magia',
+    'o que a polimerização faz?',
+    'Ela faz fusão do campo e da mão'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'master duel "efeito de monstro',
+    'quantas cartas que que desassociar do zeus para ativar seu efeito?',
+    '2)'
+)
+
+criaCartao(
+    'master duel "efeito de monstro',
+    'Qual é o efeito da "florescer de cinza ?',
+    'egar efeito de cartas " especificos)'
 )
